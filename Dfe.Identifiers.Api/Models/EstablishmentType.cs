1acaa9f77f6e9d0971fdf1a7fef7891d9c2021ca
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-03-19 16:32:19 +0000

    Add initial identifiers project

---
+++ Dfe.Identifiers.Api/Models/EstablishmentType.cs
@@ -0,0 +1,9 @@
+﻿namespace Dfe.Identifiers.Api.Models
+{
+    public class EstablishmentType
+    {
+        public long SK { get; set; }
+        public string? Name { get; set; }
+        public string? Code { get; set; }
+    }
+}
