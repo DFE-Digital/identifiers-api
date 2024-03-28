2ff527d1d49a3532301318ee2188dc4134bb76e2
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2024-03-28 11:46:52 +0000

    Update Cypress tests with requests

--- Dfe.Identifiers.Api.CypressTests/cypress/e2e/identifiers.cy.js
+++ Dfe.Identifiers.Api.CypressTests/cypress/e2e/identifiers.cy.js
@@ -1,6 +1,41 @@
 describe('Identifiers endpoint tests', () => {
 
-  it.skip('Tests the endpoint', () => {
+  const apiKey = Cypress.env('apiKey')
+  const baseUrl = Cypress.env('baseUrl')
+  const companiesHouseNumber = '11082297'
+  const ukprn = '10067112'
+  const trustReferenceNumber = 'TR03739'
 
+  context('Identifiers endpoint', () => {
+
+    const identifiersEndpoint = 'api/identifier'
+
+    it.skip('should return a list of identifiers for a Trust Reference Number', () => {
+
+      cy.api({
+        method: 'GET',
+        url: `${baseUrl}/${identifiersEndpoint}/${trustReferenceNumber}`,
+        headers: {
+          ApiKey: apiKey,
+          "Content-Type": "application/json"
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+      })
+    })
+
+    it.skip('should return a list of identifiers for a UKPRN', () => {
+
+      cy.api({
+        method: 'GET',
+        url: `${baseUrl}/${identifiersEndpoint}/${ukprn}`,
+        headers: {
+          ApiKey: apiKey,
+          "Content-Type": "application/json"
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+      })
+    })
   })
 })
