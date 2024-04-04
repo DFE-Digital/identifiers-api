72d726df0735f1ff16020f515fb53a2c18a99096
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-04-04 11:17:51 +0100

    Remove unused fields from mstr context
    models

--- Dfe.Identifiers.Api.Test/DatabaseModelBuilder.cs
+++ Dfe.Identifiers.Api.Test/DatabaseModelBuilder.cs
@@ -19,9 +19,6 @@ namespace Dfe.Identifiers.Api.Test
             result.TrustStatusId = null;
             result.RegionId = null;
             result.TrustBandingId = null;
-            result.CurrentSingleListGrouping = result.CurrentSingleListGrouping.Substring(0, 19);
-            result.FollowUpLetterSent = result.CurrentSingleListGrouping.Substring(0, 19);
-            result.PrioritisedForReview = result.PrioritisedForReview.Substring(0, 19);
             result.RID = result.RID.Substring(0, 10);
 
             return result;
