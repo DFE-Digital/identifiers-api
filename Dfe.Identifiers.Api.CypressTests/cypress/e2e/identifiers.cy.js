c6548713980d2aeb891164f0f3f7d2baa7fedc6c
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2024-04-09 13:39:14 +0100

    Expand Cypress test skeletons for all identifier options

--- Dfe.Identifiers.Api.CypressTests/cypress/e2e/identifiers.cy.js
+++ Dfe.Identifiers.Api.CypressTests/cypress/e2e/identifiers.cy.js
@@ -2,12 +2,13 @@ describe('Identifiers endpoint tests', () => {
 
   const apiKey = Cypress.env('apiKey')
   const baseUrl = Cypress.env('url')
-  const ukprn = '10067112'
-  const trustReferenceNumber = 'TR03739'
+  const identifiersEndpoint = 'api/identifier'
 
-  context('Identifiers endpoint', () => {
+  context('Trusts', () => {
 
-    const identifiersEndpoint = 'api/identifier'
+    const ukprn = '10067112'
+    const trustReferenceNumber = 'TR03739'
+    const uid = ''
 
     it.skip('should return a list of identifiers for a Trust Reference Number', () => {
 
@@ -28,6 +29,69 @@ describe('Identifiers endpoint tests', () => {
       cy.api({
         method: 'GET',
         url: `${baseUrl}/${identifiersEndpoint}/${ukprn}`,
+        headers: {
+          ApiKey: apiKey,
+          "Content-Type": "application/json"
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+      })
+    })
+
+    it.skip('should return a list of identifiers for a UID', () => {
+
+      cy.api({
+        method: 'GET',
+        url: `${baseUrl}/${identifiersEndpoint}/${uid}`,
+        headers: {
+          ApiKey: apiKey,
+          "Content-Type": "application/json"
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+      })
+    })
+  })
+
+  context('Establishments', () => {
+
+    const ukprn = ''
+    const laestab = ''
+    const urn = ''
+
+    it.skip('should return a list of identifiers for a LAESTAB', () => {
+
+      cy.api({
+        method: 'GET',
+        url: `${baseUrl}/${identifiersEndpoint}/${laestab}`,
+        headers: {
+          ApiKey: apiKey,
+          "Content-Type": "application/json"
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+      })
+    })
+
+    it.skip('should return a list of identifiers for a UKPRN', () => {
+
+      cy.api({
+        method: 'GET',
+        url: `${baseUrl}/${identifiersEndpoint}/${ukprn}`,
+        headers: {
+          ApiKey: apiKey,
+          "Content-Type": "application/json"
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+      })
+    })
+
+    it.skip('should return a list of identifiers for a URN', () => {
+
+      cy.api({
+        method: 'GET',
+        url: `${baseUrl}/${identifiersEndpoint}/${urn}`,
         headers: {
           ApiKey: apiKey,
           "Content-Type": "application/json"
