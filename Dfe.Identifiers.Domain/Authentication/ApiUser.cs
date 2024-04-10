681bf02a90a36a6b3edc181beebe9bb1acee7d0b
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-04-10 13:41:40 +0100

    Add ApiKey logic
    Middleware interecepts all requests to check it has a correct api key

---
+++ Dfe.Identifiers.Domain/Authentication/ApiUser.cs
@@ -0,0 +1,7 @@
+﻿namespace Dfe.Identifiers.Domain.Authentication;
+
+public class ApiUser
+{
+    public required string UserName { get; init; }
+    public required string ApiKey { get; init; }
+}
---
+++ Dfe.Identifiers.Domain/Authentication/AuthenticationConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Dfe.Identifiers.Domain.Authentication;
+
+public static class AuthenticationConstants
+{
+    public const string APIKEYNAME = "ApiKey";
+    public const string APIKEYCONFIGURATIONPATH = "ApiKeys";
+}
