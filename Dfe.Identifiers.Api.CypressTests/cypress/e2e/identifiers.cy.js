674cd9c8dc0af2c4f5a9d07ffa3a85a12e245984
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2024-04-10 15:33:42 +0100

    Flesh out Cypress tests

--- Dfe.Identifiers.Api.CypressTests/cypress/e2e/identifiers.cy.js
+++ Dfe.Identifiers.Api.CypressTests/cypress/e2e/identifiers.cy.js
@@ -1,104 +1,162 @@
 describe('Identifiers endpoint tests', () => {
 
-  const apiKey = Cypress.env('apiKey')
-  const baseUrl = Cypress.env('url')
+  // const apiKey = Cypress.env('apiKey')
+  const baseUrl = `${Cypress.env('url')}`
   const identifiersEndpoint = 'api/identifier'
 
   context('Trusts', () => {
 
     const ukprn = '10067112'
     const trustReferenceNumber = 'TR03739'
-    const uid = ''
+    const uid = '16917'
 
-    it.skip('should return a list of identifiers for a Trust Reference Number', () => {
+    it('should return a list of identifiers for a Trust Reference Number', () => {
 
       cy.api({
         method: 'GET',
         url: `${baseUrl}/${identifiersEndpoint}/${trustReferenceNumber}`,
         headers: {
-          ApiKey: apiKey,
+          //ApiKey: apiKey,
           "Content-Type": "application/json"
         }
       }).then((response) => {
         expect(response.status).to.eq(200)
+        expect(response.body).to.have.keys('trusts', 'establishments')
+        expect(response.body.trusts).to.have.lengthOf.at.most(1)
+        expect(response.body.trusts[0]).to.have.keys('uid', 'ukprn', 'trustReference')
+        expect(response.body.trusts[0]).to.eql({
+          'uid': uid,
+          'ukprn': ukprn,
+          'trustReference': trustReferenceNumber
+        })
+        expect(response.body.establishments).to.eql([])
       })
     })
 
-    it.skip('should return a list of identifiers for a UKPRN', () => {
+    it('should return a list of identifiers for a UKPRN', () => {
 
       cy.api({
         method: 'GET',
         url: `${baseUrl}/${identifiersEndpoint}/${ukprn}`,
         headers: {
-          ApiKey: apiKey,
+          //ApiKey: apiKey,
           "Content-Type": "application/json"
         }
       }).then((response) => {
         expect(response.status).to.eq(200)
+        expect(response.body).to.have.keys('trusts', 'establishments')
+        expect(response.body.trusts).to.have.lengthOf.at.most(1)
+        expect(response.body.trusts[0]).to.have.keys('uid', 'ukprn', 'trustReference')
+        expect(response.body.trusts[0]).to.eql({
+          'uid': uid,
+          'ukprn': ukprn,
+          'trustReference': trustReferenceNumber
+        })
+        expect(response.body.establishments).to.eql([])
       })
     })
 
-    it.skip('should return a list of identifiers for a UID', () => {
+    it('should return a list of identifiers for a UID', () => {
 
       cy.api({
         method: 'GET',
         url: `${baseUrl}/${identifiersEndpoint}/${uid}`,
         headers: {
-          ApiKey: apiKey,
+          //ApiKey: apiKey,
           "Content-Type": "application/json"
         }
       }).then((response) => {
         expect(response.status).to.eq(200)
+        expect(response.body).to.have.keys('trusts', 'establishments')
+        expect(response.body.trusts).to.have.lengthOf.at.most(1)
+        expect(response.body.trusts[0]).to.have.keys('uid', 'ukprn', 'trustReference')
+        expect(response.body.trusts[0]).to.eql({
+          'uid': uid,
+          'ukprn': ukprn,
+          'trustReference': trustReferenceNumber
+        })
+        expect(response.body.establishments).to.eql([])
       })
     })
   })
 
   context('Establishments', () => {
 
-    const ukprn = ''
-    const laestab = ''
-    const urn = ''
+    const ukprn = '10079319'
+    const laestab = '201/3614'
+    const urn = '100000'
 
-    it.skip('should return a list of identifiers for a LAESTAB', () => {
+    it('should return a list of identifiers for a LAESTAB', () => {
 
       cy.api({
         method: 'GET',
-        url: `${baseUrl}/${identifiersEndpoint}/${laestab}`,
+        url: `${baseUrl}/${identifiersEndpoint}/${sanitiseLaestab(laestab)}`,
         headers: {
-          ApiKey: apiKey,
+          //ApiKey: apiKey,
           "Content-Type": "application/json"
         }
       }).then((response) => {
         expect(response.status).to.eq(200)
+        expect(response.body).to.have.keys('trusts', 'establishments')
+        expect(response.body.establishments).to.have.lengthOf.at.most(1)
+        expect(response.body.establishments[0]).to.have.keys('laestab', 'ukprn', 'urn')
+        expect(response.body.establishments[0]).to.eql({
+          'laestab': laestab,
+          'ukprn': ukprn,
+          'urn': urn
+        })
+        expect(response.body.trusts).to.eql([])
       })
     })
 
-    it.skip('should return a list of identifiers for a UKPRN', () => {
+    it('should return a list of identifiers for a UKPRN', () => {
 
       cy.api({
         method: 'GET',
         url: `${baseUrl}/${identifiersEndpoint}/${ukprn}`,
         headers: {
-          ApiKey: apiKey,
+          //ApiKey: apiKey,
           "Content-Type": "application/json"
         }
       }).then((response) => {
         expect(response.status).to.eq(200)
+        expect(response.body).to.have.keys('trusts', 'establishments')
+        expect(response.body.establishments).to.have.lengthOf.at.most(1)
+        expect(response.body.establishments[0]).to.have.keys('laestab', 'ukprn', 'urn')
+        expect(response.body.establishments[0]).to.eql({
+          'laestab': laestab,
+          'ukprn': ukprn,
+          'urn': urn
+        })
+        expect(response.body.trusts).to.eql([])
       })
     })
 
-    it.skip('should return a list of identifiers for a URN', () => {
+    it('should return a list of identifiers for a URN', () => {
 
       cy.api({
         method: 'GET',
         url: `${baseUrl}/${identifiersEndpoint}/${urn}`,
         headers: {
-          ApiKey: apiKey,
+          //ApiKey: apiKey,
           "Content-Type": "application/json"
         }
       }).then((response) => {
         expect(response.status).to.eq(200)
+        expect(response.body).to.have.keys('trusts', 'establishments')
+        expect(response.body.establishments).to.have.lengthOf.at.most(1)
+        expect(response.body.establishments[0]).to.have.keys('laestab', 'ukprn', 'urn')
+        expect(response.body.establishments[0]).to.eql({
+          'laestab': laestab,
+          'ukprn': ukprn,
+          'urn': urn
+        })
+        expect(response.body.trusts).to.eql([])
       })
     })
   })
 })
+
+function sanitiseLaestab(laestab) {
+  return laestab.replace('/', '%2F')
+}
