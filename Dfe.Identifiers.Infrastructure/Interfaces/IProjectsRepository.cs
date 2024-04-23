90928b43de79cb45accbb623ed4d8fab80b1b096
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-04-23 10:42:41 +0100

    Create the project repository

---
+++ Dfe.Identifiers.Infrastructure/Interfaces/IProjectsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Dfe.Identifiers.Domain.Models;
+
+namespace Dfe.Identifiers.Infrastructure.Interfaces;
+
+public interface IProjectsRepository
+{
+    Task<List<ConversionProject>> GetConversionProjectsByIdentifier(string identifier,
+        CancellationToken cancellationToken);
+
+    Task<List<TransferProject>> GetTransfersProjectsByIdentifier(string identifier,
+        CancellationToken cancellationToken);
+
+    Task<List<FormAMatProject>> GetFormAMatProjectsByIdentifier(string identifier,
+        CancellationToken cancellationToken);
+}
