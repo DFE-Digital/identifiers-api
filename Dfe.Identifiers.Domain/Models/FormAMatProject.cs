4a29291dc22dcfdaab4cf19c758888b1ae612ac7
Nick Warms <Nicholas.WARMS@education.gov.uk>
2024-04-23 10:41:37 +0100

    Add Project models

---
+++ Dfe.Identifiers.Domain/Models/FormAMatProject.cs
@@ -0,0 +1,8 @@
+﻿namespace Dfe.Identifiers.Domain.Models;
+
+public class FormAMatProject
+{
+    public int? Id { get; set; }
+    public string? ApplicationReference { get; set; }
+    public string? ReferenceNumber { get; set; }
+}
---
+++ Dfe.Identifiers.Domain/Models/TransferProject.cs
@@ -0,0 +1,9 @@
+﻿namespace Dfe.Identifiers.Domain.Models;
+
+public class TransferProject
+{
+    public int? Id { get; set; }
+    public int URN { get; set; }
+    public string? ProjectReference { get; set; }
+    public long? OutgoingTrustUKPRN { get; set; }
+}
