9d8f99e3bc6050335bf311ccc0df833f91f127b7
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-07-24 17:16:16 +0100

    Set custom CDN waf rules
    * this will be used for specific cases where we want requests to bypass the rate limit policy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -334,3 +334,19 @@ variable "private_endpoint_configurations" {
   }))
   default = {}
 }
+
+variable "cdn_frontdoor_waf_custom_rules" {
+  description = "Map of all Custom rules you want to apply to the CDN WAF"
+  type = map(object({
+    priority : number,
+    action : string
+    match_conditions : map(object({
+      match_variable : string,
+      match_values : optional(list(string), []),
+      operator : optional(string, "Any"),
+      selector : optional(string, null),
+      negation_condition : optional(bool, false),
+    }))
+  }))
+  default = {}
+}
